a796ee93fa9bbe4c58fa15043a99fb1e0989df57
Felix Hamel <fhamel@optimaweb.ca>
2014-09-18 15:06:29 -0400

    Ajout des clases de l'enonce

---
+++ src/ift287/tp2/entities/Carte.java
@@ -0,0 +1,7 @@
+package ift287.tp2.entities;
+
+public class Carte {
+	private String titreCarte;
+	private String nomEquipe;
+	private int anneeSortie = 0;
+}
---
+++ src/ift287/tp2/entities/Joueur.java
@@ -0,0 +1,7 @@
+package ift287.tp2.entities;
+
+public class Joueur {
+	private String cle;
+	private String nomJoueur;
+	private int nombreCartes = 0;
+}
