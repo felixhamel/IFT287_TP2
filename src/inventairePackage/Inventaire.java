562b48d8ad9cbf288f708fe5a7d470f0cf2e51e5
Felix Hamel <felixhamel@me.com>
2014-10-02 14:16:50 -0400

    Ajout du debut de la DOC

--- src/inventairePackage/Inventaire.java
+++ src/inventairePackage/Inventaire.java
@@ -15,8 +15,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Inventaire
